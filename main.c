2bdbe147385e153de42ca0b49dbea150681064a0
Pedro Rodolfo Silva Galvão Santos <110983216+Pedro22D0W@users.noreply.github.com>
2023-10-31 11:40:45 -0300

    buuble_sort

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <time.h>
 
+
+
 clock_t start(){
     return clock();
 }
@@ -24,19 +26,77 @@ void registrar_tempo(clock_t start, clock_t end, char *nome_arquivo){
 typedef struct info{
     int key;
     float value;
-}Info;
+}info;
+
+int vet_tam[5];
+
+int tam = 5;
+info* cria_vetor(int seed,int tam){
+
+srand(seed);
+
+info *vetor =(info*)malloc(sizeof(info)*tam);
+vetor[0].key= (rand())%10;
+
+for(int i=1;i<tam;i++){
+
+    vetor[i].key= (rand())%10 + vetor[i-1].key;
+    vetor[i].value= 100 + (rand())%10;
+
+};
+
+
+return vetor;
+
+}
+
+void buuble(int tam,int seed){
+
+    info* vetor = cria_vetor(seed,tam);
+
+    for (int i = 0; i < tam; i++)
+    {
+        printf("\n %d",vetor[i].key);
+    }
+
+
+    for (int i = 0; i < tam; i++)
+    {
+        for (int i = 0; i < tam; i++)
+        {
+          int n = vetor[i + 1].key;
+
+          if (n>vetor[i].key && (i+1)!=tam)
+          {
+            vetor[i+1].key = vetor[i].key;
+            vetor[i].key = n;
+          }
+
+        }
+
+    }
+    printf("\n//////////////////////////////////////////\n");
+
+      for (int i = 0; i < tam; i++)
+    {
+        printf("\n%d",vetor[i].key);
+    }
+
+}
 
 int main(){
-    int vet[5];
-    int vet2[5];
-    srand(73);
-    for(int i = 0; i < 5; i++){
-        vet[i] = rand();
-        vet2[i] = rand();
-    }
-    for (int i = 0; i < 5; i++){
-        printf("%d - %d \n", vet[i], vet2[i]);
-    }
+
+
+   // int vet[5];
+  //  int vet2[5];
+   // srand(73);
+  //  for(int i = 0; i < 5; i++){
+   //     vet[i] = rand();
+   //     vet2[i] = rand();
+   // }
+   // for (int i = 0; i < 5; i++){
+    //   printf("%d - %d \n", vet[i], vet2[i]);
+    //}
 
 
     // clock_t start, end;
