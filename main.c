26bc0e6f18949993c6f0355067b64fdf94d94dd1
Pedro22D0W <pedro.rodolfo.galvao22@hotmail.com>
2023-11-04 19:31:35 -0300

    Correções nos metodos bubble e cocktail

--- main.c
+++ main.c
@@ -36,28 +36,23 @@ void printar_vetor(Info *vetor, int tam){
     }
 }
 
-void bubble_sort(Info *vetor, int tam, int i, FILE *ARQ){
+void bubble_sort(Info *vetor, int tam, int i, FILE *ARQ) {
     clock_t start, end;
 
     start = clock();
-    for (int i = 0; i < tam; i++)
-    {
-        for (int i = 0; i < tam; i++)
-        {
-          int n = vetor[i + 1].key;
-
-          if (n>vetor[i].key && (i+1)!=tam)
-          {
-            vetor[i+1].key = vetor[i].key;
-            vetor[i].key = n;
-          }
+    for (int i = 0; i < tam - 1; i++) {
+        for (int j = 1; j < tam - i ; j++) {
+            if (vetor[j].key < vetor[j + 1].key) {
+                int temp = vetor[j].key;
+                vetor[j].key = vetor[j + 1].key;
+                vetor[j + 1].key = temp;
+            }
         }
     }
     end = clock();
 
-    fprintf(ARQ, "[%d] - %d ", i+1, (end - start));
+    fprintf(ARQ, "[%d] - %d ", tam, (end - start));
 }
-
 
 
 void insertion_sort(Info *vetor, int tam, int i, FILE *ARQ){
@@ -85,30 +80,28 @@ void cocktail_sort(Info *vetor, int tam, int i, FILE *ARQ) {
 
     start = clock();
     int aux;
-    int bottom = 0;
-    int top = tam - 1;
+    int lim_inf = 0;
+    int lim_sup = tam - 1;
     int swapped = 0;
-    while(swapped == 0 && bottom < top)//Se não houver troca de posições ou o ponteiro que
-    {                                   //sobe ultrapassar o que desce, o vetor esta ordenado
-        swapped = 1;
-        //Este for é a “ida” para a direita
-        for(i = bottom; i < top; i = i + 1)
+    while(swapped == 0 && lim_inf < lim_sup)
+    {                                   
+        swapped = 1;//se não houver troca, swapped não é atualizado no loop o vetor está ordenado
+
+        
+        for(i = lim_inf; i < lim_sup; i++)//for da direita para a esquerda
         {
-            if(vetor[i].key > vetor[i + 1].key)   //indo pra direita:testa se o próximo é maior
-            {   //indo pra direita:se o proximo é maior que o atual,
-                //troca as posições
+            if(vetor[i].key < vetor[i + 1].key) //se limite inferior for menor que limite inferior + 1, troca
+            {   
                 aux=vetor[i].key;
                 vetor[i].key=vetor[i+1].key;
                 vetor[i+1].key=aux;
                 swapped = 0;
             }
-        }//fecha for
-        // diminui o  `top` porque o elemento com o maior valor
-        // já está na direita (atual posição top)
-        top = top - 1;
-        //Este for é a “ida” para a esquerda
-        for(i = top; i > bottom; i = i - 1)
-        {  if(vetor[i].key < vetor[i - 1].key)
+        }
+        lim_sup--; //diminui o limite superior porque o maior valor já está na posição cprreta 
+        
+        for(i = lim_sup; i > lim_inf; i--)//for da esquerda para a direita
+        {  if(vetor[i].key > vetor[i - 1].key)//se limite superior for maior que limite superior - 1, troca
             {
                 aux=vetor[i].key;
                 vetor[i].key=vetor[i-1].key;
@@ -116,14 +109,13 @@ void cocktail_sort(Info *vetor, int tam, int i, FILE *ARQ) {
                 swapped = 0;
             }
         }
-        //aumenta o `bottom` porque o menor valor já está
-        //na posição inicial (bottom)
-        bottom = bottom + 1;
-    }//fecha while
+       
+        lim_inf++; //aumenta o limite inferior porque o menor valor já está na posição correta
+    }
     end = clock();
 
     fprintf(ARQ, "[%d] - %d ", i+1, (end - start));
- }// fim da funçao
+ }
 
 
 int main(){
@@ -176,6 +168,7 @@ int main(){
         for(int j = 0; j < 10; j++){
             Info *vetor = criar_vetor_tipo1(tam[i], seeds[j]);
             cocktail_sort(vetor, tam[i], j, ARQ);
+            printar_vetor(vetor,tam[i]);
             free(vetor);
         }
 
@@ -187,6 +180,6 @@ int main(){
         }
         fprintf(ARQ, "\n");
     }
-    
+
     return 0;
 }
